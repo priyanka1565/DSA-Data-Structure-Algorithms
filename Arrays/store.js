5106257e322a2871117d6e9713e6b1830e658e42
priyanka1565 <priyankaingle250@gmail.com>
2023-09-01 10:18:34 +0530

    acessing and modifying aarays items aadded

---
+++ Arrays/store.js
@@ -0,0 +1,12 @@
+//suppose we want to store the shopping list in the arrays
+const shopping_list = ['bread', 'milk', 'butter', 'cheese', 'maggy', 'biscuit'];
+console.log(shopping_list);
+
+//finding the length of the array
+var length_of_array = shopping_list.length;
+console.log(length_of_array);
+
+//acessing and modifying the arrays items
+shopping_list[0] = "paneer";
+console.log(shopping_list)
+
